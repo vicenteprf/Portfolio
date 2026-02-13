a902ad584db15e0c1231bf21a2bdc8ff59ddf91a
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-02-13 14:11:22 -0300

    atualizações no html e responsividade

---
+++ script.js
@@ -0,0 +1,13 @@
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const secao = document.querySelectorAll(".secao");
+
+secao.forEach((Element) => myObserver.observe(Element));
