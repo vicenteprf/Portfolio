89a15ec2d27e4d9acc74f6e349d3be32b94fa2fb
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-03-17 10:49:44 -0300

    melhora navegação com scroll suave e centralizado

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ const myObserver = new IntersectionObserver((entries) => {
 const secao = document.querySelectorAll(".secao");
 
 secao.forEach((Element) => myObserver.observe(Element));
+
+const btnSobre = document.querySelector("#btn-sobre");
+
+btnSobre.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const section = document.querySelector("#sobre");
+
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
+
+const btnProjetos = document.querySelector("#btn-projetos");
+
+btnProjetos.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const section = document.querySelector("#container-projetos");
+
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
